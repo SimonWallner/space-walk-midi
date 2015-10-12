d5618a9c339be7372f34a29d812c0a0d4df6aa5f
Simon Wallner <me@simonwallner.at>
2015-10-12 11:38:41 +0200

    first working version, though I already checked it in...

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # space-walk-midi
 a midi controlled remote tweaking plugin for Space Walk
+
+Find the github pages hosted version here: http://simonwallner.github.io/space-walk-midi/
--- midi.js
+++ midi.js
@@ -1,6 +1,7 @@
 var libsw = new LibSpaceWalk();
 
 var midi;
+var tweakableVariables;
 
 var mapping = {
     "1": "slider1",
@@ -40,7 +41,86 @@ function onMIDIMessage(message) {
     var value = data[2];
     var id = mapping[note];
     if (id) {
-        $('#' + id).slider('value', value);
+        var min = $('#' + id).slider('option', 'min');
+        var max = $('#' + id).slider('option', 'max');
+        $('#' + id).slider('value', linearMap(0, 127, min, max, value));
+    }
+}
+
+function linearMap(a, b, r, s, value) {
+    if (a == b)
+    {
+        if (value <= a)
+        {
+            return r;
+        }
+        else
+        {
+            return s;
+        }
+    }
+
+    var ratio = (value - a) / (b - a);
+    return r + (s - r) * ratio;
+}
+
+
+libsw.onMessage = function(data) {
+	if (data.type === 'com.beyond35000.remote.list') {
+        var controllerNumber = parseInt(data.payload.controllerNumber);
+		tweakableVariables = [];
+
+        data.payload.forEach(function(obj) {
+            obj.fields.forEach(function(variable){
+                    tweakableVariables.push({
+                        objectName: obj.name,
+                        variableName: variable.name,
+                        value: variable.value,
+                        min: variable.value / 4,
+                        max: variable.value * 4
+                    })
+            })
+        });
+
+        tweakableVariables.forEach(function(element, index) {
+            $('<option>', {
+                text: element.objectName + '::' + element.variableName,
+                'data-index': index
+            }).appendTo('.control select');
+        })
+	}
+}
+
+libsw.onSessionStarted = function() {
+    $('option').remove();
+    $('<option>', {
+        text: '--'
+    }).appendTo('select');
+
+}
+
+var sliderCallback = function(event, ui) {
+    $(this).siblings('.value').text(ui.value);
+
+    var index = $(this).data('index');
+    if (index >= 0) {
+        var variable = tweakableVariables[$(this).data('index')]
+        var message = {
+            type: 'com.beyond35000.remote.list',
+            payload: [
+                {
+                    name: variable.objectName,
+                    fields: [
+                        {
+                            name: variable.variableName,
+                            type: 'single',
+                            value: ui.value
+                        }
+                    ]
+                 }
+            ]
+        };
+        libsw.postMessage(message);
     }
 }
 
@@ -49,8 +129,33 @@ $(document).ready(function() {
         $('#slider' + i).slider({
             min: 0,
             max: 127,
-            value: 0
+            value: 0,
+            change: sliderCallback,
+            slide: sliderCallback
         });
+
+        $('#select' + i).change(function() {
+            if (this.selectedIndex != 0) {
+                var index = $($(this).children()[this.selectedIndex]).data('index');
+                var element = tweakableVariables[index];
+
+                $(this).siblings('.value').text(element.value);
+                $(this).siblings('.from').attr('value', element.min);
+                $(this).siblings('.to').attr('value', element.max);
+
+                var slider = $(this).siblings('div');
+                slider.attr('data-index', index);
+                slider.slider({
+                    min: element.min,
+                    max: element.max,
+                    value: element.value
+                })
+            } else {
+                $(this).siblings('div').attr('data-index', -1);
+                $(this).siblings('.from').text('--');
+                $(this).siblings('.to').text('--');
+            }
+        })
     }
 
     if (navigator.requestMIDIAccess) {
